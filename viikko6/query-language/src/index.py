46b1d59d909be1a0db9fa2d15554147e764f4721
Janne Kärki <janne.karki@helsinki.fi>
2022-12-12 15:21:22 +0200

    Ladattu tehtävä uudestaan

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,9 +1,9 @@
-from statistics import Statistics
+from statistics_1 import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn
 
 def main():
-    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
